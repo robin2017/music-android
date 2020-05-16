b193e130099462d88df206e5ff3e785757d748d2
robin2017 <robin.seu@foxmail.com>
2020-05-16 15:42:36 +0800

    add log

---
+++ app/src/main/java/com/example/musicdemo/activity/LogonActivity.java
@@ -0,0 +1,16 @@
+package com.example.musicdemo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.musicdemo.R;
+
+public class LogonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logon);
+    }
+}
